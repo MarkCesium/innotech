d6b11265b7cdd0e77499616236c33a06c754a101
MarkCesium <danikhrenkov29@gmail.com>
2026-09-17 17:42:50 +0300

    feat(users): add find user method, handle integrity error

--- src/modules/users/repository.py
+++ src/modules/users/repository.py
@@ -1,19 +1,33 @@
 from uuid import UUID
 
+from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.sql import update
 
+from .exceptions import UserAlreadyExistsError
 from .models import User
 
 
-async def get_user(session: AsyncSession, id: UUID) -> User | None:
+async def get_user_by_id(session: AsyncSession, id: UUID) -> User | None:
     return await session.get(User, id)
+
+
+async def find_user_or_none(session: AsyncSession, **filter_by) -> User | None:
+    statement = select(User).filter_by(**filter_by)
+    result = await session.execute(statement)
+    return result.scalar_one_or_none()
 
 
 async def create_user(session: AsyncSession, **kwargs):
     user = User(**kwargs)
     session.add(user)
-    await session.flush()
+    try:
+        await session.flush()
+    except IntegrityError as e:
+        if "unique constraint" in str(e).lower():
+            raise UserAlreadyExistsError("User with this email already exists") from e
+        raise
     await session.refresh(user)
     return user
 
--- src/modules/users/service.py
+++ src/modules/users/service.py
@@ -1,17 +1,20 @@
 from uuid import UUID
 
 from .models import User
-from .repository import create_user, get_user, update_user
+from .repository import create_user, find_user_or_none, get_user_by_id, update_user
 
 
 class UserService:
     def __init__(self, session):
         self.session = session
 
+    async def get_user_by_email(self, email: str) -> User | None:
+        return await find_user_or_none(self.session, email=email)
+
     async def create_user(self, email: str, hashed_password: str) -> User:
         return await create_user(self.session, email=email, hashed_password=hashed_password)
 
     async def activate_user(self, id: UUID) -> None:
-        user = await get_user(self.session, id)
+        user = await get_user_by_id(self.session, id)
         if user:
             await update_user(self.session, id, is_active=True)
