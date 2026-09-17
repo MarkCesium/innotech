4e590a08570c5bb07f4b7114ea18bafc064e17c8
MarkCesium <danikhrenkov29@gmail.com>
2026-09-17 15:17:57 +0300

    feat(db): add try/except block to session dependency

--- src/core/db.py
+++ src/core/db.py
@@ -20,4 +20,14 @@ async_session_factory = async_sessionmaker[AsyncSession](
 )
 
 
+async def get_session():
+    async with async_session_factory() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+
+
 class BaseModel(DeclarativeBase): ...
