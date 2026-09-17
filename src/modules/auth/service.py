aaba85d14e72722b66b45781124546790bcb0778
MarkCesium <danikhrenkov29@gmail.com>
2026-09-17 19:58:10 +0300

    feat(auth): complete register method

--- src/modules/auth/service.py
+++ src/modules/auth/service.py
@@ -1,16 +1,15 @@
-from sqlalchemy.ext.asyncio import AsyncSession
-
+from src.core.config import JWTConfig
 from src.modules.users.service import UserService
 
-from .security import hash_password
+from .security import create_verification_token, hash_password
 
 
 class AuthService:
-    def __init__(self, session: AsyncSession, user_service: UserService):
-        self.session = session
+    def __init__(self, user_service: UserService, jwt_config: JWTConfig):
         self.users = user_service
+        self.jwt_config = jwt_config
 
-    async def register_user(self, email: str, password: str) -> None:
+    async def register_user(self, email: str, password: str) -> str:
         hashed_password = await hash_password(password)
-        await self.users.create_user(email, hashed_password)
-        # TODO: Send notification
+        user = await self.users.create_user(email, hashed_password)
+        return create_verification_token(user.id.hex, self.jwt_config)
