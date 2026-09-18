79d31bbe58eeed7a8e3b0d805f27fbc1703daa96
MarkCesium <danikhrenkov29@gmail.com>
2026-09-18 20:27:04 +0300

    refactor: move metadata registration from core to migrations

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,8 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from src.core.config import settings
+from src.core.db import BaseModel  # noqa: F401
+from src.modules.users.models import User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +23,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from src.core.models import BaseModel
 
 target_metadata = BaseModel.metadata
 
--- src/core/models.py
+++
@@ -1,3 +0,0 @@
-from src.modules.users.models import User  # noqa: F401
-
-from .db import BaseModel  # noqa: F401
